1d04670e6db08c255d4594f46d7beeed5df7b363
stewie-gil <gilliganstewie7@gmail.com>
2023-07-02 19:25:19 +0300

    fixed db connection

--- version2/backend/api/server.js
+++ version2/backend/api/server.js
@@ -1,34 +1,34 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const apiRoutes = require('./routes/api');
 
 app.use(express.json());
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 
-const mongoURI = process.env.MONGODB_URI;
+ 
 
-async function startServer() {
-  try {
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log('Connected to MongoDB');
-    
-    app.listen(3001, () => {
-      console.log('Server is running on port 3001');
-    });
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+
+const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(process.env.MONGO_URI);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
   }
-}
 
-startServer();
+  connectDB().then(() => {
+app.listen(3001, ()=>{
+    console.log('Server is running on port 3001');
+});
+})
 
-app.use('/api', apiRoutes);
+const apiRoutes = require('./routes/api');
+
+app.use('/api', apiRoutes)
