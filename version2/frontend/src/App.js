4e4d65239bb13af1d63cb628ae57a7a228d653c1
stewie-gil <gilliganstewie7@gmail.com>
2023-07-02 15:50:52 +0300

    commiting the shell of my version2

--- version2/frontend/src/App.js
+++ version2/frontend/src/App.js
@@ -1,103 +1,25 @@
-import React, { useState } from 'react';
-import {useEffect} from 'react';
-import Header from './components/header';
-import Footer from './components/Footer';
-import SearchBox from './components/SearchBox';
-import PropertyListing from './components/PropertyListing';
-import styles from './components/Footer.module.css';
-import axios from 'axios';
-import Bar from './components/bar'
-
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
-const [listings, setlistings] = useState([]);
-const [searchQuery, setSearchQuery] = useState('');
-const [searchListings, setSearchListings] = useState([]);
-
-useEffect(() => {
-    axios.get('http://localhost:3001/api/properties')
-        .then(response => {
-         const dbData = response.data;
-         setlistings(dbData)
-         setSearchListings(dbData)
- 
-        })
-         .catch(error => {
-          console.error('Error fetching data:', error);
-     });
-         }, []);
-    
-
-
-const handleSearch = async(searchQuery) => {
-try {
-
-    const response = await axios.get(`http://localhost:3001/api/properties/${searchQuery}`);
-    const filteredListings = response.data;
-    setSearchListings(filteredListings);
- 
-    if (searchQuery) {
-        console.log('current query', searchQuery);
-    } else {
-              
-        setSearchListings(listings);
-       
-        console.log('going back to original data', searchQuery);
-    }
-} catch(error) {
-    console.error('Error search listings:', error);
-}
-};
-
-
-//<PropertyListing listings={searchListings.length > 0 ? searchListings : listings} />
-
-
-useEffect(() =>{
-function handleScroll() {
-    console.log('scrolled');
-    const footer = document.querySelector(`.${styles.footer}`);
-    const isScrolledToBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight;
-
-    if(isScrolledToBottom) {
-        footer.style.display = 'block';
-    } else {
-        footer.style.display = 'none';
-    }
-}
-
-window.addEventListener('scroll', handleScroll);
-
-
-
-return () => {
-    window.removeEventListener('scroll', handleScroll);
-};
-
-}, []);
-
-
-
-return (
-
-
-
-<div>
-<Header/>
-<SearchBox
- searchQuery={searchQuery}
- setSearchQuery={setSearchQuery}
- handleSearch={handleSearch}
-/>
-<Bar/>
-<PropertyListing listings={searchListings}/>
-<Footer/>
-</div>
-
-);
-
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
