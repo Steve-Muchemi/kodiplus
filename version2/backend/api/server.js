d1e35c6d7c813957989e8acc8a6673e239802b44
stewie-gil <gilliganstewie7@gmail.com>
2023-07-02 19:03:01 +0300

    added a wait while connecting to mongodb

--- version2/backend/api/server.js
+++ version2/backend/api/server.js
@@ -1,29 +1,34 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const apiRoutes = require('./routes/api');
 
 app.use(express.json());
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
-
-const mongoURI = process.env.MONGODB_URI 
-
-mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(()=> {
-console.log('connected to MongoDB');
-})
-.catch((error)=>{
-    console.error('Error connecting to MongoDB');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 });
 
-app.listen(3001, ()=>{
-    console.log('Server is running on port 3001');
-});
+const mongoURI = process.env.MONGODB_URI;
 
-const apiRoutes = require('./routes/api');
+async function startServer() {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('Connected to MongoDB');
+    
+    app.listen(3001, () => {
+      console.log('Server is running on port 3001');
+    });
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+}
 
-app.use('/api', apiRoutes)
+startServer();
+
+app.use('/api', apiRoutes);
