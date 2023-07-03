b2733aeaa893c16c121346f0c4deb9bd91a18b14
stewie-gil <gilliganstewie7@gmail.com>
2023-07-03 15:16:02 +0300

    path

--- version2/backend/api/server.js
+++ version2/backend/api/server.js
@@ -1,51 +1,39 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const PORT = 3001
+const PORT = 3001;
 const path = require('path');
-
 
 app.use(express.json());
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
-
- 
- 
-  
-  const connectDB = async () => {
-    try {
-      const conn = await mongoose.connect(process.env.MONGO_URI);
-      console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  }
-  
-  //Routes go here
-  //app.all('*', (req,res) => {
-  //    res.json({"every thing":"is awesome"})
-  //})
-const apiRoutes = require('./routes/api');
-  app.use('/api', apiRoutes);
-
-app.use(express.static(path.join(__dirname, ".../frontend/build")))
-
-app.get("*", (req, res)=>{
-res.sendFile(path.join(__dirname, ".../frontend/build", "index.html"));
-
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 });
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '.../frontend/build', 'index.html'));
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+const apiRoutes = require('./routes/api');
+app.use('/api', apiRoutes);
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
+
+//Connect to the database before listening
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("listening for requests");
   });
-  //Connect to the database before listening
-  connectDB().then(() => {
-      app.listen(PORT, () => {
-          console.log("listening for requests");
-      })
-  })
+});
