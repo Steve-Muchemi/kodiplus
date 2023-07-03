1e01baeb482616197d7768c2bba2cd0b09a268c4
stewie-gil <gilliganstewie7@gmail.com>
2023-07-03 14:49:58 +0300

    adding path

--- version2/backend/api/server.js
+++ version2/backend/api/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
- const PORT = 3001
+const PORT = 3001
+const path = require('path');
+
+
 app.use(express.json());
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -33,9 +36,13 @@ const apiRoutes = require('./routes/api');
 app.use(express.static(path.join(_dirname, "../frontend/build")))
 
 app.get("*", (req, res)=>{
-res.sendFile(path.join(_dirname, "../frontend/build/index.html"));
+res.sendFile(path.join(_dirname, "../frontend/build", "index.html"));
 
 });
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+  });
   //Connect to the database before listening
   connectDB().then(() => {
       app.listen(PORT, () => {
