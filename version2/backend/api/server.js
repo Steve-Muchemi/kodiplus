aca6d85232c432787d6e2a8771f8fd7051c4c9b3
stewie-gil <gilliganstewie7@gmail.com>
2023-07-03 12:26:17 +0300

    debug

--- version2/backend/api/server.js
+++ version2/backend/api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
- const PORT = process.env.PORT || 3000
+ const PORT = 3000
 app.use(express.json());
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -18,7 +18,7 @@ app.use(function(req, res, next) {
       const conn = await mongoose.connect(uri);
       console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-      console.log(error);
+      console.log("error from mongo connection",error);
       process.exit(1);
     }
   }
