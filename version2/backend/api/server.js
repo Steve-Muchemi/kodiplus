cb4064d3058c9ed9f9df83acf67757049406b4ee
stewie-gil <gilliganstewie7@gmail.com>
2023-07-03 12:19:04 +0300

    debugging connection

--- version2/backend/api/server.js
+++ version2/backend/api/server.js
@@ -11,11 +11,11 @@ app.use(function(req, res, next) {
   });
 
  
- 
+ const uri = "mongodb+srv://stewie-gil:777Stephen!@cluster0.ez5jfzu.mongodb.net/?retryWrites=true&w=majority";
   
   const connectDB = async () => {
     try {
-      const conn = await mongoose.connect(process.env.MONGO_URI);
+      const conn = await mongoose.connect(uri);
       console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
       console.log(error);
