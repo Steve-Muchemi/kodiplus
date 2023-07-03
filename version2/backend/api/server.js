259813defbcecf8108d58b0ded699e5a74f84ee5
stewie-gil <gilliganstewie7@gmail.com>
2023-07-03 14:58:16 +0300

    typofix

--- version2/backend/api/server.js
+++ version2/backend/api/server.js
@@ -33,15 +33,15 @@ app.use(function(req, res, next) {
 const apiRoutes = require('./routes/api');
   app.use('/api', apiRoutes);
 
-app.use(express.static(path.join(__dirname, "../frontend/build")))
+app.use(express.static(path.join(__dirname, ".../frontend/build")))
 
 app.get("*", (req, res)=>{
-res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+res.sendFile(path.join(__dirname, ".../frontend/build", "index.html"));
 
 });
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '.../frontend/build', 'index.html'));
   });
   //Connect to the database before listening
   connectDB().then(() => {
