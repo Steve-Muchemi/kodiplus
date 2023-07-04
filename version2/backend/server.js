065eaf3e0242668cc8a954a12efab5e0c6d88cce
stewie-gil <gilliganstewie7@gmail.com>
2023-07-04 03:35:02 +0300

    fixing CORS

--- version2/backend/server.js
+++ version2/backend/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 app.use(express.json());
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'https://kodiplus.cyclic.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
