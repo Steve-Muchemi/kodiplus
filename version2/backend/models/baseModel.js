aeaac91d2750ad438f984cedb260b48def81f79b
stewie-gil <gilliganstewie7@gmaail.co>
2023-07-11 08:00:56 -0700

    fixed indentation and spaces

--- version2/backend/models/baseModel.js
+++ version2/backend/models/baseModel.js
@@ -2,24 +2,24 @@
 const mongoose = require('mongoose');
 const baseModelSchema = new mongoose.Schema({
 
-    propertyType: {
-        type: String, 
-        required: true,
-    }, 
-    price: {
-        type: Number, 
-        required: true, 
-    }, 
-    description: String, 
-    propertyName : String,
-    location: String, 
-    amenities: [{
-        type: String,
-    }], 
-    contactInfo: [{
-        type: String, 
-    }], 
-    imageUrls: [String],
+  propertyType: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  description: String,
+  propertyName: String,
+  location: String,
+  amenities: [{
+    type: String
+  }],
+  contactInfo: [{
+    type: String
+  }],
+  imageUrls: [String]
 });
 
-module.exports = mongoose.model('baseModel', baseModelSchema)
+module.exports = mongoose.model('baseModel', baseModelSchema);
